be93d7c59e6341fbb2a207ea70132c3abe3aa23e
Samuel Kwak <kwak123@gmail.com>
2018-01-09 16:37:36 -0500

    basic backend packages and ang components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TaskComponent } from './task/task.component';
+import { TrackerComponent } from './tracker/tracker.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent,
+    TrackerComponent
   ],
   imports: [
     BrowserModule
